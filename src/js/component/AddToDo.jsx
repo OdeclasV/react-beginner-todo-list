bbaeaa265b8c6aed917799ea1007dc4288d3484c
Victor H. Salcedo <victorsalc2@gmail.com>
2021-09-28 23:58:36 +0000

    add styling

--- src/js/component/AddToDo.jsx
+++ src/js/component/AddToDo.jsx
@@ -6,45 +6,44 @@ export const AddToDo = () => {
 
 	return (
 		<>
-			<div>
-				<input
-					type="text"
-					placeholder="Add Todo item"
-					value={newTodo}
-					onChange={e => setNewTodo(e.target.value)}
-					onKeyUp={e => {
-						if (e.key === "Enter") {
-							if (e.target.value != "") {
-								//let newList = listOfToDos.concat([newTodo]);
-								let newList = [...listOfToDos, newTodo]; // adding all elements of listoftodos, and then newTodo
-								setListOfToDos(newList);
-								setNewTodo("");
-							}
-						}
-					}}
-				/>
-				<ul>
-					{listOfToDos.map((todo, i) => {
-						return (
-							<li key={i}>
-								{todo}
-								<button
-									onClick={() => {
-										let newList = listOfToDos.filter(
-											(item, index) => {
-												return i !== index;
-											}
-										);
-										setListOfToDos(newList);
-									}}>
-									X
-								</button>
-							</li>
-						);
-					})}
-				</ul>
-				<div>Total Todos: {listOfToDos.length}</div>
-			</div>
+			<input
+				type="text"
+				placeholder="Add Todo item"
+				value={newTodo}
+				onChange={e => setNewTodo(e.target.value)}
+				onKeyUp={e => {
+					if (e.key === "Enter" && e.target.value != "") {
+						//let newList = listOfToDos.concat([newTodo]);
+						let newList = [...listOfToDos, newTodo]; // adding all elements of listoftodos, and then newTodo
+						setListOfToDos(newList);
+						setNewTodo("");
+					}
+				}}
+			/>
+			<ul className="list-group list-group-flush">
+				{listOfToDos.map((todo, i) => {
+					return (
+						<li
+							key={i}
+							className="list-group-item d-flex justify-content-between align-items-center">
+							{todo}
+							<button
+								className="btn btn"
+								onClick={() => {
+									let newList = listOfToDos.filter(
+										(item, index) => {
+											return i !== index;
+										}
+									);
+									setListOfToDos(newList);
+								}}>
+								X
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+			<div className="todoCounts">Total Todos: {listOfToDos.length}</div>
 		</>
 	);
 };
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,8 +4,8 @@ import { AddToDo } from "./AddToDo.jsx";
 //create your first component
 const Home = () => {
 	return (
-		<div className="container-fluid text-center mt-5">
-			<h1>Hello Hello</h1>
+		<div className="container-fluid mt-5 text-center">
+			<h1>To-Dos</h1>
 			<AddToDo />
 		</div>
 	);
